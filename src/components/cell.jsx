3c8c90d2bfda475e0fbeabe5a5af429989a5ea15
kirangowda95-tech <76085458+KiranGowda95-tech@users.noreply.github.com>
2024-06-17 09:53:38 +0000

    updated

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -1,8 +1,13 @@
-function Cell({filled,onClick})={
-    return <button
-     type='button' 
-     onClick={onClick}
-      className={filled ? 'cell cell-activated':'cell'} />
+function Cell({ filled, onClick, isDisabled, label }) {
+  return (
+    <button
+      disabled={isDisabled}
+      type="button"
+      aria-label={label}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
 }
 
-export default Cell
+export default Cell;
